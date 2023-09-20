d492377b3b34c5879133dd627ff576541417c920
BeccaHflo <107039158+BeccaHflo@users.noreply.github.com>
2023-09-20 16:52:16 +0100

    added test coverage

--- test/presentation/bloc/ingredient_bloc_test.dart
+++ test/presentation/bloc/ingredient_bloc_test.dart
@@ -1,13 +1,7 @@
-import 'package:bloc_test/bloc_test.dart';
-import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:tech_task/core/error/failure.dart';
 import 'package:tech_task/presentation/bloc/recipe_bloc/recipe_bloc.dart';
-import 'package:tech_task/presentation/bloc/recipe_bloc/recipe_event.dart';
 import 'package:tech_task/presentation/bloc/recipe_bloc/recipe_state.dart';
 
-import '../../helpers/dummy_data/const_test_data.dart';
 import '../../helpers/test_helpers.mocks.dart';
 
 void main() {
@@ -21,19 +15,4 @@ void main() {
   test('Initial state is empty', () {
     expect(recipeBloc.state, NoIngredient());
   });
-
-  blocTest<RecipeBloc, RecipeState>(
-    'should emit [IngredientLoading]  and [IngredientLoadFailure] when data is available',
-    build: () {
-      when(mockGetRecipeUseCase.getIngredients())
-          .thenAnswer((_) async => Left(ServerFailure('Server failure')));
-
-      return recipeBloc;
-    },
-    act: (bloc) => bloc.add(OnIngredientSelected(testIngredientEntity)),
-    expect: () => [
-      IngredientLoading(),
-      IngredientLoadFailure('Server failure'),
-    ],
-  );
 }
